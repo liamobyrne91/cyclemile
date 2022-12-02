46fa511df98edf28194245080af7b5f0874ca005
Liam O'Byrne <108347646+liamobyrne91@users.noreply.github.com>
2022-12-02 17:18:27 +0000

    Create jquery.slider.js

---
+++ media/js/jquery.slider.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function () {
+    $("#slide1").click(function () {
+        $("#info").slideToggle("slow");
+    });
+});
+$(document).ready(function () {
+    $("#slide2").click(function () {
+        $("#history").slideToggle("slow");
+    });
+});
